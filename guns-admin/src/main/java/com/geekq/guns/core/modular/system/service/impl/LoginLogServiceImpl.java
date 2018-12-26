f3b47b0c0a47f19aea9bdc8ad699670a9575a119
qiurunze <qiurunze@youxin.com>
2018-12-26 19:16:22 +0800

    初始化

---
+++ guns-admin/src/main/java/com/geekq/guns/core/modular/system/service/impl/LoginLogServiceImpl.java
@@ -0,0 +1,28 @@
+package com.geekq.guns.core.modular.system.service.impl;
+
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.geekq.guns.core.modular.system.dao.LoginLogMapper;
+import com.geekq.guns.core.modular.system.model.LoginLog;
+import com.geekq.guns.core.modular.system.service.ILoginLogService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ * 登录记录 服务实现类
+ * </p>
+ *
+ * @author geekq
+ * @since 2018-02-22
+ */
+@Service
+public class LoginLogServiceImpl extends ServiceImpl<LoginLogMapper, LoginLog> implements ILoginLogService {
+
+    @Override
+    public List<Map<String, Object>> getLoginLogs(Page<LoginLog> page, String beginTime, String endTime, String logName, String orderByField, boolean asc) {
+        return this.baseMapper.getLoginLogs(page, beginTime, endTime, logName, orderByField, asc);
+    }
+}
---
+++ guns-admin/src/main/java/com/geekq/guns/core/util/KaptchaUtil.java
@@ -0,0 +1,16 @@
+package com.geekq.guns.core.util;
+
+import com.geekq.guns.core.config.properties.GunsProperties;
+
+/**
+ * 验证码工具类
+ */
+public class KaptchaUtil {
+
+    /**
+     * 获取验证码开关
+     */
+    public static Boolean getKaptchaOnOff() {
+        return SpringContextHolder.getBean(GunsProperties.class).getKaptchaOpen();
+    }
+}
